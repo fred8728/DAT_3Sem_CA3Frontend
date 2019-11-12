f8be0be64051665cb9c2d8cd36446139fb80bc5a
Emil-Skovbo <emilskovbo18@gmail.com>
2019-11-12 13:50:53 +0100

    pushing frontend

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react"
+import facade from "./apiFacade";
+
+class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" }
+  }
+  login = (evt) => {
+    evt.preventDefault();
+    this.props.login(this.state.username, this.state.password);
+  }
+  onChange = (evt) => {
+    this.setState({ [evt.target.id]: evt.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.login} onChange={this.onChange} >
+          <input placeholder="User Name" id="username" />
+          <input placeholder="Password" id="password" />
+          <button>Login</button>
+        </form>
+      </div>
+    )
+  }
+}
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataFromServer: "Fetching!!" };
+  }
+  componentDidMount() { 
+    facade.fetchData().then(res=> this.setState({dataFromServer: res}));
+  }
+  render() {
+    return (
+      <div>
+        <h2>Data Received from server</h2>
+        <h3>{this.state.dataFromServer}</h3>
+      </div>
+    )
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false }
+  }
+
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  } //TODO
+  login = (user, pass) => {
+    facade.login(user, pass)
+      .then(res => this.setState({ loggedIn: true }));
+  } //TODO
+  render() {
+    return (
+      <div>
+        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
+          (<div>
+            <LoggedIn />
+            <button onClick={this.logout}>Logout</button>
+          </div>)}
+      </div>
+    )
+  }
+
+}
+export default App;
+
---
+++ src/apiFacade.js
@@ -0,0 +1,58 @@
+const URL = "http://localhost:8080/securitystarter";
+function handleHttpErrors(res) {
+    if (!res.ok) {
+        return Promise.reject({ status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
+class ApiFacade {
+    //Insert utility-methods from a latter step (d) here
+    makeOptions(method, addToken, body) {
+        var opts = {
+            method: method,
+            headers: {
+                "Content-type": "application/json",
+                'Accept': 'application/json',
+            }
+        }
+        if (addToken && this.loggedIn()) {
+            opts.headers["x-access-token"] = this.getToken();
+        }
+        if (body) {
+            opts.body = JSON.stringify(body);
+        }
+        return opts;
+    }
+    setToken = (token) => {
+        localStorage.setItem('jwtToken', token)
+    }
+    getToken = () => {
+        return localStorage.getItem('jwtToken')
+    }
+    loggedIn = () => {
+        const loggedIn = this.getToken() != null;
+        return loggedIn;
+    }
+    logout = () => {
+        localStorage.removeItem("jwtToken");
+    }
+
+    login = (user, pass) => {
+        const options = this.makeOptions("POST", true, { username: user, password: pass });
+        return fetch(URL + "/api/login", options)
+            .then(handleHttpErrors)
+            .then(res => { this.setToken(res.token) })
+    }
+    fetchData = () => {
+        const options = this.makeOptions("GET", true); //True add's the token
+        return fetch(URL + "/api/info/user", options).then(handleHttpErrors);
+    }
+
+
+
+}
+const facade = new ApiFacade();
+export default facade;
+
+
